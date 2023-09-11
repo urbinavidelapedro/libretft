d0f6963d7130003c3213d0f5910690c015b8de81
nemo <urbinavidelapedro@gmail.com>
2023-09-11 13:42:08 -0300

    se añade codigo js para que el carrusel tenga un boton funciona a base de un switch y ocultar mostrar

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var botonSiguiente = document.getElementById("siguiente");
+
+botonSiguiente.onclick = function() {
+  // Obtén la imagen actualmente visible
+  var imagenActual = document.querySelector(".imagen[style='display: block;']");
+
+  // Compara su id con los casos del switch
+  switch (imagenActual.id) {
+    case "imagenUno":
+      document.getElementById("imagenUno").style.display = "none";
+      document.getElementById("imagenDos").style.display = "block";
+      break;
+    case "imagenDos":
+      document.getElementById("imagenDos").style.display = "none";
+      document.getElementById("imagenTres").style.display = "block";
+      break;
+    case "imagenTres":
+      document.getElementById("imagenTres").style.display = "none";
+      document.getElementById("imagenCuatro").style.display = "block";
+      break;
+    case "imagenCuatro":
+      document.getElementById("imagenCuatro").style.display = "none";
+      document.getElementById("imagenCinco").style.display = "block";
+      break;
+    case "imagenCinco":
+      document.getElementById("imagenCinco").style.display = "none";
+      document.getElementById("imagenSeis").style.display = "block";
+      break;
+    case "imagenSeis":
+      document.getElementById("imagenSeis").style.display = "none";
+      document.getElementById("imagenUno").style.display = "block";
+      break;
+    default:
+      // Acciones predeterminadas si no coincide con ningún caso
+      break;
+  }
+};
