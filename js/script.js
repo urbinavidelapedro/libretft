2924902dc2994d06d925a0a16f1a5bd85f0f1c98
Molde Maestro <urbinavidelapedro@gmail.com>
2023-10-05 20:52:03 +0200

    se añaden efectos esteticos

--- js/script.js
+++ js/script.js
@@ -1,37 +1,57 @@
-var botonSiguiente = document.getElementById("siguiente");
+// document.addEventListener('DOMContentLoaded', function() {
 
-botonSiguiente.onclick = function() {
-  // Obtén la imagen actualmente visible
-  var imagenActual = document.querySelector(".imagen[style='display: block;']");
 
-  // Compara su id con los casos del switch
-  switch (imagenActual.id) {
-    case "imagenUno":
-      document.getElementById("imagenUno").style.display = "none";
-      document.getElementById("imagenDos").style.display = "block";
-      break;
-    case "imagenDos":
-      document.getElementById("imagenDos").style.display = "none";
-      document.getElementById("imagenTres").style.display = "block";
-      break;
-    case "imagenTres":
-      document.getElementById("imagenTres").style.display = "none";
-      document.getElementById("imagenCuatro").style.display = "block";
-      break;
-    case "imagenCuatro":
-      document.getElementById("imagenCuatro").style.display = "none";
-      document.getElementById("imagenCinco").style.display = "block";
-      break;
-    case "imagenCinco":
-      document.getElementById("imagenCinco").style.display = "none";
-      document.getElementById("imagenSeis").style.display = "block";
-      break;
-    case "imagenSeis":
-      document.getElementById("imagenSeis").style.display = "none";
-      document.getElementById("imagenUno").style.display = "block";
-      break;
-    default:
-      // Acciones predeterminadas si no coincide con ningún caso
-      break;
-  }
-};
+// document.getElementById("imagenUno").style.display = "Block";
+// document.getElementById("imagenDos").style.display = "none";
+// document.getElementById("imagenTres").style.display = "none";
+// document.getElementById("imagenCuatro").style.display = "none";
+// document.getElementById("imagenCinco").style.display = "none";
+// document.getElementById("imagenSeis").style.display = "none";
+
+
+
+// var botonSiguiente = document.getElementById("siguiente");
+
+// botonSiguiente.onclick = function() {
+//   // Obtén la imagen actualmente visible
+//   var imagenes = document.querySelector(".divCaja");
+//   var imagenVisible = document.querySelector(".divCaja[style*='display: block;']");
+
+//   console.log(imagenVisible);
+
+  
+
+ 
+//   // Compara su id con los casos del switch
+//   switch (imagenVisible.id) {
+//     case "imagenUno":
+//       document.getElementById("imagenUno").style.display = "none";
+//       document.getElementById("imagenDos").style.display = "block";
+//       break;
+//     case "imagenDos":
+//       document.getElementById("imagenDos").style.display = "none";
+//       document.getElementById("imagenTres").style.display = "block";
+//       break;
+//     case "imagenTres":
+//       document.getElementById("imagenTres").style.display = "none";
+//       document.getElementById("imagenCuatro").style.display = "block";
+//       break;
+//     case "imagenCuatro":
+//       document.getElementById("imagenCuatro").style.display = "none";
+//       document.getElementById("imagenCinco").style.display = "block";
+//       break;
+//     case "imagenCinco":
+//       document.getElementById("imagenCinco").style.display = "none";
+//       document.getElementById("imagenSeis").style.display = "block";
+//       break;
+//     case "imagenSeis":
+//       document.getElementById("imagenSeis").style.display = "none";
+//       document.getElementById("imagenUno").style.display = "block";
+//       break;
+//     default:
+//       // Acciones predeterminadas si no coincide con ningún caso
+//       break;
+//   }
+// };
+
+// });
